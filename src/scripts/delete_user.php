47a119c08f3f3dfa7e35025ad8e86f05e6d6538a
Jorge Higuera <jorge.higuera.fombuena@gmail.com>
2016-12-05 11:59:03 +0100

    Delete user script

---
+++ src/scripts/delete_user.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ . '/../../bootstrap.php';
+
+
+if ($argc < 2) {
+    echo "$argv[0] <id_user>" . PHP_EOL;
+    exit();
+}
+
+$entityManager = getEntityManager();
+
+$userRepository = $entityManager->getRepository('MiW16\Results\Entity\User');
+
+/** @var \MiW16\Results\Entity\User $user */
+$user = $userRepository->findOneById($argv[1]);
+
+if($user) {
+    $entityManager->remove($user);
+    $entityManager->flush();
+}
+else {
+    echo 'Usuario no encontrado';
+}
