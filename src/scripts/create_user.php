0b1d0c2a87b848a547a9c8dc36b55756258d9756
Jorge Higuera <jorge.higuera.fombuena@gmail.com>
2016-12-05 11:18:07 +0100

    Script para crear usuario

---
+++ src/scripts/create_user.php
@@ -0,0 +1,21 @@
+<?php
+require_once __DIR__ . '/../../bootstrap.php';
+
+
+if ($argc < 3) {
+    echo "$argv[0] <nombre_usuario> <email> <password>" . PHP_EOL;
+    exit();
+}
+
+$entityManager = getEntityManager();
+
+$userRepository = $entityManager->getRepository('MiW16\Results\Entity\User');
+
+/** @var \MiW16\Results\Entity\User $user */
+$user = new \MiW16\Results\Entity\User();
+$user->setUsername($argv[1]);
+$user->setEmail($argv[2]);
+$user->setPassword($argv[3]);
+
+$entityManager->persist($user);
+$entityManager->flush();
