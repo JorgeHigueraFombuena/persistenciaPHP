f318e20ea25ade7a2915658f8bd63c431f144e18
Jorge Higuera <jorge.higuera.fombuena@gmail.com>
2016-12-05 12:01:05 +0100

    Update user script

---
+++ src/scripts/update_user.php
@@ -0,0 +1,27 @@
+<?php
+require_once __DIR__ . '/../../bootstrap.php';
+
+
+if ($argc < 2) {
+    echo "$argv[0] <id_user> <user_name> <email> <password>" . PHP_EOL;
+    exit();
+}
+
+$entityManager = getEntityManager();
+
+$userRepository = $entityManager->getRepository('MiW16\Results\Entity\User');
+
+/** @var \MiW16\Results\Entity\User $user */
+$user = $userRepository->findOneById($argv[1]);
+if($user) {
+    echo json_encode($user, JSON_PRETTY_PRINT);
+    $user->setUsername($argv[2]);
+    $user->setEmail($argv[3]);
+    $user->setPassword($argv[4]);
+    $entityManager->persist($user);
+    $entityManager->flush();
+    echo json_encode($user, JSON_PRETTY_PRINT);
+}
+else {
+    echo 'Usuario no encontrado';
+}
